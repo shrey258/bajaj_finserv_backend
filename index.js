7b58ef7f394dfe007ef2cb0fa618d512a2b71a58
Shreyansh Gupta <hardikshrey123@gmail.com>
2024-08-02 20:05:24 +0530

    Cors3

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan('combined'));
-app.use(cors());
+app.use(cors({
+  origin: 'https://bajaj-finserv-frontend-eight.vercel.app'
+}));
 
 app.post('/bfhl', (req, res) => {
   const { data } = req.body;
